946060aaf5d2162e6bb347652f97fb07e0fe63a2
Sanjeev Pandey <sanjeev.pandey@jupiter.money>
2020-09-15 12:24:05 +0530

    added custom rule association for web acl

--- main.tf
+++ main.tf
@@ -52,6 +52,20 @@ resource "aws_wafv2_web_acl" "main" {
             }
           }
         }
+
+        dynamic "rule_group_reference_statement" {
+          for_each = length(lookup(rule.value, "rule_group_reference_statement", {})) == 0 ? [] : [lookup(rule.value, "rule_group_reference_statement", {})]
+          content {
+            arn        = lookup(rule_group_reference_statement.value, "arn")
+
+            dynamic "excluded_rule" {
+              for_each = length(lookup(rule_group_reference_statement.value, "excluded_rule", {})) == 0 ? [] : toset(lookup(rule_group_reference_statement.value, "excluded_rule"))
+              content {
+                name = excluded_rule.value
+              }
+            }
+          }
+        }
       }
 
       dynamic "visibility_config" {
